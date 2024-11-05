af926e7e7c369d319253742112b5ea42a9d1599e
Nadya <166049745+viunadyapage@users.noreply.github.com>
2024-11-05 15:25:49 +0700

    Update TokoSerbaAda.java

--- 23142_Nadya_Praktikum_Tugas7/TokoSerbaAda.java
+++ 23142_Nadya_Praktikum_Tugas7/TokoSerbaAda.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg23142_Nadya_Praktikum_Tugas7;
 
 import java.util.Scanner;
